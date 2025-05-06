151da620156a1d96983f4832272705b328cb0653
Nahom Zewdu <nahiyo86@gmail.com>
2025-05-06 18:17:38 +0300

    Test script for job result in-memory storage

--- test_jobs.py
+++ test_jobs.py
@@ -2,18 +2,24 @@
 
 from nuvom import task
 from nuvom.worker import start_worker_pool
+from nuvom.result_store import get_result, get_error
+
 import threading
 
 @task
-def greet(name):
-    print(f"Hello, {name}")
+def add(a, b):
+    print("Hello, You")
+    return (a + b)
 
 # Start worker threads
 threading.Thread(target=start_worker_pool, daemon=True).start()
 
 # Now delay
-greet.delay("Shady")
+job_id = add.delay(2, 4)
+
 
 # Let the main thread sleep to allow processing
 import time
 time.sleep(5)
+
+print(get_result(job_id))
