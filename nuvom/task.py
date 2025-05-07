4f8b75976ab0866e1e463702cd0180c02e491ed8
Nahom Zewdu <nahiyo86@gmail.com>
2025-05-07 16:20:16 +0300

    Add a bach submission support.

--- nuvom/task.py
+++ nuvom/task.py
@@ -6,8 +6,8 @@
 # nuvom/task.py
 
 import functools
-from .job import Job
-from .queue import get_global_queue
+from nuvom.job import Job
+from nuvom.queue import get_global_queue
 
 _TASK_REGISTRY = {}
 
@@ -31,8 +31,19 @@ class Task:
     
     def submit(self, *args, **kwargs):
         return self.delay(*args, **kwargs)
+    
+    def map(self, arg_tuples):
+        """
+        Enqueue multiple jobs in batch using a list of argument tuples.
+        """
+        job_ids = []
+        for args in arg_tuples:
+            if not isinstance(args, (list, tuple)):
+                raise TypeError("Each map item must be a tuple or list of arguments")
+            job_ids.append(self.delay(*args))
 
-
+        return job_ids
+    
 def task(_func=None, *, name=None, retries=0):
     def wrapper(func):
         return Task(func, name=name, retries=retries)
