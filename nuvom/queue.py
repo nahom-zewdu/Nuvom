56fb4ea17d752cec4fcc03631655db27223b6ba3
Nahom Zewdu <nahiyo86@gmail.com>
2025-05-10 22:40:09 +0300

    Serialize data before pushing them to a queue and deserialize them after dequeue

--- nuvom/queue.py
+++ nuvom/queue.py
@@ -5,22 +5,28 @@
 import queue
 import threading
 
-# Global default queue (used by @task.delay)
+from nuvom.serialize import get_serializer
+from nuvom.job import Job  # make sure this path is correct
+
 _GLOBAL_QUEUE = None
 _GLOBAL_LOCK = threading.Lock()
 
 
 class JobQueue:
     def __init__(self, maxsize=0):
-        self.q = queue.Queue(maxsize=maxsize)  # 0 = infinite size
-        self.lock = threading.Lock()  # for batch pop
+        self.q = queue.Queue(maxsize=maxsize)
+        self.lock = threading.Lock()
+        self.serializer = get_serializer()
 
-    def enqueue(self, job):
-        self.q.put(job)
+    def enqueue(self, job: Job):
+        serialized = self.serializer.serialize(job.to_dict())
+        self.q.put(serialized)
 
-    def dequeue(self, timeout=1):
+    def dequeue(self, timeout=1) -> Job | None:
         try:
-            return self.q.get(timeout=timeout)
+            data = self.q.get(timeout=timeout)
+            job_dict = self.serializer.deserialize(data)
+            return Job.from_dict(job_dict)
         except queue.Empty:
             return None
 
@@ -29,8 +35,9 @@ class JobQueue:
         with self.lock:
             for _ in range(batch_size):
                 try:
-                    job = self.q.get(timeout=timeout)
-                    batch.append(job)
+                    data = self.q.get(timeout=timeout)
+                    job_dict = self.serializer.deserialize(data)
+                    batch.append(Job.from_dict(job_dict))
                 except queue.Empty:
                     break
         return batch
