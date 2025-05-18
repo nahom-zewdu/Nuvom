6536c5db143e49d9386a87cde922b487c2020a27
nahom-zewdu <nahiyo86@gmail.com>
2025-05-19 00:09:29 +0300

    Update Job class to include lifecycle hook fields

--- nuvom/task.py
+++ nuvom/task.py
@@ -4,20 +4,39 @@
 
 
 # nuvom/task.py
-
+from typing import Callable, Optional, Any
 import functools
+import warnings
+
 from nuvom.job import Job
 from nuvom.queue import get_queue_backend
 
 _TASK_REGISTRY = {}
 
 class Task:
-    def __init__(self, func, name=None, retries=0, store_result=True, timeout_secs=None):
+    def __init__(self, 
+                 func, 
+                 name=None, 
+                 retries=0, 
+                 store_result=True, 
+                 timeout_secs=None,
+                 before_job: Optional[Callable[[], None]] = None, 
+                 after_job: Optional[Callable[[Any], None]] = None, 
+                 on_error: Optional[Callable[[Exception], None]] = None
+                 ):
         self.func = func
         self.name = name or func.__name__
+        if self.name.startswith("test_"):
+            warnings.warn(f"Task '{self.name}' may shadow pytest test function collection", stacklevel=2)
+        
         self.retries = retries
         self.store_result = store_result
         self.timeout_secs = timeout_secs
+        
+        self.before_job = before_job
+        self.after_job = after_job
+        self.on_error = on_error
+        
         _TASK_REGISTRY[self.name] = self
 
     def __call__(self, *args, **kwargs):
@@ -29,6 +48,9 @@ class Task:
                   retries=self.retries, 
                   store_result=self.store_result, 
                   timeout_secs=self.timeout_secs,
+                  before_job = self.before_job,
+                  after_job = self.after_job,
+                  on_error = self.on_error,
                 )
         queue = get_queue_backend()
         queue.enqueue(job)
@@ -51,9 +73,9 @@ class Task:
 
         return jobs
     
-def task(_func=None, *, name=None, retries=0, store_result=True):
+def task(_func=None, *, name=None, retries=0, store_result=True, before_job: Optional[Callable[[], None]] = None, after_job: Optional[Callable[[Any], None]] = None, on_error: Optional[Callable[[Exception], None]] = None):
     def wrapper(func):
-        return Task(func, name=name, retries=retries, store_result=store_result)
+        return Task(func, name=name, retries=retries, store_result=store_result, before_job = before_job, after_job = after_job, on_error = on_error,)
 
     if _func is None:
         return wrapper
