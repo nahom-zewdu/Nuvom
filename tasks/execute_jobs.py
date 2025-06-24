d4a7004b223810e8a7c55f89b3d319af77f0b93b
nahom-zewdu <nahiyo86@gmail.com>
2025-06-24 19:37:22 +0300

    update(tasks): print job id after queuing

--- tasks/execute_jobs.py
+++ tasks/execute_jobs.py
@@ -2,4 +2,5 @@
 from tasks.jobs import add
 
 jobs = add.map([(2, 3), (3,3), (4,4), (5,5), (6,11), (2, 11), (3,11), (4,11), (5,11), (6,11)])
-print(f"Job queued with ID: {jobs}")
+for job in jobs:
+    print(f"Job queued with ID: {job.id }")
